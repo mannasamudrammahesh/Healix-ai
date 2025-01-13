cef5dbe7968dd83db4f0c38f652c16fde0b12124
Muni Mahesh <153695240+mannasamudrammahesh@users.noreply.github.com>
2025-01-13 13:15:23 +0530

    Update confetti.tsx

--- components/confetti.tsx
+++ components/confetti.tsx
@@ -1,54 +1,34 @@
-
-import { useCallback, useEffect, useRef } from 'react';
-
+import React, { useCallback, useRef, useEffect } from 'react';
 import ReactCanvasConfetti from 'react-canvas-confetti';
 
 export default function Confetti() {
   const refAnimationInstance = useRef(null);
 
-  const getInstance = useCallback(instance => {
+  const getInstance = useCallback((instance) => {
     refAnimationInstance.current = instance;
   }, []);
 
   const makeShot = useCallback((particleRatio, opts) => {
-    refAnimationInstance.current &&
+    if (refAnimationInstance.current) {
       refAnimationInstance.current({
         ...opts,
         origin: { y: 0.7 },
-        particleCount: Math.floor(200 * particleRatio)
+        particleCount: Math.floor(200 * particleRatio),
       });
+    }
   }, []);
 
-  useEffect(() => fire(), []);
-
   const fire = useCallback(() => {
-    makeShot(0.25, {
-      spread: 26,
-      startVelocity: 55
-    });
-
-    makeShot(0.2, {
-      spread: 60
-    });
-
-    makeShot(0.35, {
-      spread: 100,
-      decay: 0.91,
-      scalar: 0.8
-    });
-
-    makeShot(0.1, {
-      spread: 120,
-      startVelocity: 25,
-      decay: 0.92,
-      scalar: 1.2
-    });
-
-    makeShot(0.1, {
-      spread: 120,
-      startVelocity: 45
-    });
+    makeShot(0.25, { spread: 26, startVelocity: 55 });
+    makeShot(0.2, { spread: 60 });
+    makeShot(0.35, { spread: 100, decay: 0.91, scalar: 0.8 });
+    makeShot(0.1, { spread: 120, startVelocity: 25, decay: 0.92, scalar: 1.2 });
+    makeShot(0.1, { spread: 120, startVelocity: 45 });
   }, [makeShot]);
+
+  useEffect(() => {
+    fire();
+  }, [fire]);
 
   return (
     <ReactCanvasConfetti
@@ -59,8 +39,9 @@ export default function Confetti() {
         width: '100%',
         height: '100%',
         top: 0,
-        left: 0
+        left: 0,
       }}
     />
   );
 }
+
