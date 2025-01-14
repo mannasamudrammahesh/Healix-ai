f06e639b55ad9d53d528927aec1e23747a744112
Muni Mahesh <153695240+mannasamudrammahesh@users.noreply.github.com>
2025-01-15 00:12:09 +0530

    Update route.ts

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,7 +1,12 @@
 import { createNextRouteHandler } from "uploadthing/next";
- 
 import { ourFileRouter } from "./core";
  
 export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
 });
+
+// utils/uploadthing.ts
+import { generateComponents } from "@uploadthing/react";
+import type { OurFileRouter } from "@/app/api/uploadthing/core";
+ 
+export const { UploadButton, UploadDropzone, Uploader } = generateComponents<OurFileRouter>();
