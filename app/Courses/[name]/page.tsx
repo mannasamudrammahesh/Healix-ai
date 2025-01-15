fcaca490a5fd23393e76df9c97a97e361029c922
Muni Mahesh <153695240+mannasamudrammahesh@users.noreply.github.com>
2025-01-15 23:02:50 +0530

    Update page.tsx

--- app/Courses/[name]/page.tsx
+++ app/Courses/[name]/page.tsx
@@ -14,7 +14,7 @@ import { useRef } from "react";
 import { useRive, RiveState, useStateMachineInput, StateMachineInput, Layout, Fit, Alignment, RiveProps } from 'rive-react';
 import styles from '@/styles/styles.module.css'
 import "@/styles/LoginFormComponent.css";
-import Confetti from "../../../../../components/Confetti";
+import Confetti from "components/Confetti";
 
 import toast, {Toaster} from "react-hot-toast";
 
