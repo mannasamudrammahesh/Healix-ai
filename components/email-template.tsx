176360d6831a617fd41262dacbade2b652ce54ce
Muni Mahesh <153695240+mannasamudrammahesh@users.noreply.github.com>
2025-02-11 19:47:44 +0530

    Update email-template.tsx

--- components/email-template.tsx
+++ components/email-template.tsx
@@ -34,7 +34,7 @@ export const ContactEmailTemplate: React.FC<Readonly<ContactEmailTemplateProps>>
   message
 }) => (
   <div>
-    <h1>You got a message from Mentify</h1>
+    <h1>You got a message from Healix</h1>
     <h2>Name: {name}</h2>
     <h2>Email: {email}</h2>
     <h2>Message: {message}</h2>
