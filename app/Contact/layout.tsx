400fda98a8a531ac491124d0c9ad460c0b478359
Muni Mahesh <153695240+mannasamudrammahesh@users.noreply.github.com>
2025-02-12 11:07:41 +0530

    Update layout.tsx

--- app/Contact/layout.tsx
+++ app/Contact/layout.tsx
@@ -25,7 +25,7 @@ export default function HomeLayout({
                     <div className='flex items-center gap-3'>
                         <Link href="/" className='flex items-center '>
 <Image src="/logo.svg" alt="Healix Logo" width={35} height={35} className='mr-[0.8px]'/>
-                            <h1 className='text-xl'>entify</h1>
+                            <h1 className='text-xl'>ealix</h1>
                         </Link>
                     </div>
 
