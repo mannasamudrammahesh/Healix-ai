4f5e9c6f3b981eae211c4f5488f20f6f8f3a55f2
Muni Mahesh <153695240+mannasamudrammahesh@users.noreply.github.com>
2025-03-04 17:54:22 +0530

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyAx34o31vs5bNBpR8BbftYHU-hC4jqOOJQ");  // API key included
+const genAI = new GoogleGenerativeAI("AIzaSyB3uzzN1gNaaPZywn-vmO7kicvLmqtmt-8");  // API key included
 const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 const chat = model.startChat({
   history: [],
